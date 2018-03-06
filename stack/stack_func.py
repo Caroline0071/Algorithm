96126ca6de12eaa87a8cbaea612660ed600be02c
zhangyi9 <zhangyi9@fans-MacBook-Air.local>
2018-03-07 00:42:40 +0800

    add stack

---
+++ stack/stack_func.py
@@ -0,0 +1,38 @@
+'''
+使用list实现栈
+'''
+
+class Stack(list):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, elememt):
+        self.items.append(elememt)
+
+    def pop(self):
+        if not self.isEmpty():
+            self.items.pop()
+        else:
+            raise Exception("Stack is empty")
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return (len(self.items))
+
+def main():
+    stack_a = Stack()
+    stack_a.push(1)
+    stack_a.push(2)
+    stack_a.push(3)
+    print(stack_a.size())
+    print(stack_a.peek())
+    print(stack_a.pop())
+    print(stack_a.peek())
+
+if __name__ == '__main__':
+    main()
